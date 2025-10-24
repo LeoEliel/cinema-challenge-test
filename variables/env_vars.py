1662f938fd6e885375085976ff01927091e2644f
Leonardo Eliel <leoeliel13@gmail.com>
2025-10-24 17:57:10 -0400

    chore(setup): inicializa estrutura do projeto, gitignore, requirements e variáveis [CN-1]

--- variables/env_vars.py
+++ variables/env_vars.py
@@ -1,5 +1,5 @@
 # A URL base da sua API (quando rodando localmente)
-API_LOCAL_URL = "http://localhost:3000/api/v1"
+API_BASE_URL = "http://localhost:3000/api/v1"
 
 # A URL base do seu Front-end (quando rodando localmente)
-UI_LOCAL_URL = "http://localhost:3002/"
+UI_BASE_URL = "http://localhost:3002/"
